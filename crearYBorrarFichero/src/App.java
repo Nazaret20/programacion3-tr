11f56375e9ded6e805da6f65b0de31a55ef66652
Nazaret <nazaretgutirrezmartn@gmail.com>
2026-04-13 14:04:54 +0200

    ejercicio 13A

--- crearYBorrarFichero/src/App.java
+++ crearYBorrarFichero/src/App.java
@@ -2,25 +2,55 @@ import java.io.File;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        /*Desarrolla un programa que cree un archivo llamado "miArchivo.txt" y luego lo elimine. El programa debe informar en cada paso si la operación fue exitosa o no. */
+        /*
+         * Desarrolla un programa que cree un archivo llamado "miArchivo.txt" y luego lo
+         * elimine. El programa debe informar en cada paso si la operación fue exitosa o
+         * no.
+         */
 
+        
+        try {
+            File archivo = new File("crearYBorrarFichero/miArchivo.txt");
 
-        File archivo = new File("miArchivo.txt");
-        File rutaArchivo = new File("C:\\Users\\Nazaret\\Documents\\programacion\\vscode\\3ºTr\\crearYBorrarFichero");
+            if (archivo.createNewFile()) {
+                System.out.println("El archivo se ha creado");
+            } else {
+                System.out.println("Error al crear archivo");
+            }
 
-        if (archivo.exists()) {
-            System.out.println("El archivo se ha creado");
-        } else {
-            System.out.println("Error al crear archivo");
+            System.out.println(archivo.getName());
+            recorrerDirectorio(archivo.getPath());
+
+            if (archivo.delete()) {
+                System.out.println("El archivo se eliminó correctamente");
+            } else {
+                System.out.println("Error al eliminar el archivo");
+            }
+        } catch (Exception e) {
+           System.out.println("Ha ocurrido un fallo");
         }
 
-        System.out.println(archivo);
+    }
 
-       if (archivo.exists()) {
-            archivo.delete();
-            System.out.println("El archivo se eliminó correctamente");
-       } else {
-            System.out.println("Error al eliminar el archivo");
-       }
+     public static void recorrerDirectorio(String ruta) {
+        File fichero = new File(ruta);
+
+        if (fichero.isDirectory() && fichero.exists()) {
+            File[] archivos = fichero.listFiles();
+
+            for (File archivo : archivos) {
+                if (archivo.isDirectory()) {
+                    System.out.println("+ " + archivo.getName().toUpperCase());
+                    recorrerDirectorio(archivo.getPath());
+                } else {
+                    System.out.println("\t - " + archivo.getName().toLowerCase());
+                }
+            }
+            
+        } else {
+            System.out.println("Error, inválido");
+        }
+
+        
     }
 }
