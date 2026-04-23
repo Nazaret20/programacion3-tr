e7ff5e0c00a4300905fbc3cc0a7b225e894213c9
Nazaret <nazaretgutirrezmartn@gmail.com>
2026-04-23 11:16:21 +0200

    ejercicios 23A

---
+++ BBDD/temperaturas/src/main/java/com/prog/App.java
@@ -0,0 +1,52 @@
+package com.prog;
+
+import java.sql.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Connection conexion = openConnection();
+        System.out.println("Conectado a la BBDD");
+
+       // Preparamos la consulta
+        Statement s = conexion.createStatement();
+        ResultSet rs = s.executeQuery("SELECT * FROM test");
+
+        // Iteramos sobre todos los registros del resultado
+        while (rs.next()) {
+            // Podemos acceder por nombre de columna o por índice
+            System.out.println(rs.getString("nombre") + " (" + rs.getInt("edad") + ")");
+        }
+
+        s.executeUpdate("UPDATE test SET nombre = 'Marta' WHERE nombre = 'Juan'");
+
+        System.out.println("Después del update");
+        rs = s.executeQuery("SELECT * FROM test");
+
+        while (rs.next()) {
+            // Podemos acceder por nombre de columna o por índice
+            System.out.println(rs.getString("nombre") + " (" + rs.getInt("edad") + ")");
+        }
+        
+
+        conexion.close();
+    }
+
+    public static Connection openConnection() {
+        Connection con = null;
+        try {
+            // Creamos la URL de conexión
+            // Formato:
+            // jdbc:mariadb://servidor:puerto/nombreBaseDatos?usuario=xxx&contraseña=xxx
+            String connectionUrl = "jdbc:mariadb://localhost:3306/bdprueba1?user=root&password=nazaret";
+
+            // Obtenemos el objeto Connection que representa la conexión
+            con = DriverManager.getConnection(connectionUrl);
+
+        } catch (SQLException e) {
+            // Capturamos errores relacionados con SQL y la base de datos
+            System.out.println("Excepción SQL: " + e.getMessage());
+        }
+    
+        return con;
+    }
+}
