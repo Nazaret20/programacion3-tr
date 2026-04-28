8316c0643988ed4622c8d3e3f92f356ba6088de0
Nazaret <nazaretgutirrezmartn@gmail.com>
2026-04-28 20:44:10 +0200

    ejercicio 28A

--- BBDD/universidad/src/main/java/com/prog/Curso.java
+++ BBDD/universidad/src/main/java/com/prog/Curso.java
@@ -1,5 +1,108 @@
 package com.prog;
 
+import java.time.LocalDateTime;
+
 public class Curso {
+    private int id;
+    private String clave, descripcion, nombre, semestre;
+    private int creditos;
+    private boolean activo;
+    private LocalDateTime created_at, update_at;
+   
+    public Curso(String clave, String nombre, String descripcion, int creditos, String semestre) {
+        this.clave = clave;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.creditos = creditos;
+        this.semestre = semestre;
+        this.activo = true; 
+    }
+
+    public Curso(int id, String clave, String descripcion, String nombre, String semestre, int creditos, boolean activo,
+            LocalDateTime created_at, LocalDateTime update_at) {
+        this.id = id;
+        this.clave = clave;
+        this.descripcion = descripcion;
+        this.nombre = nombre;
+        this.semestre = semestre;
+        this.creditos = creditos;
+        this.activo = activo;
+        this.created_at = created_at;
+        this.update_at = update_at;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(String semestre) {
+        this.semestre = semestre;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public LocalDateTime getUpdate_at() {
+        return update_at;
+    }
+
+    public void setUpdate_at(LocalDateTime update_at) {
+        this.update_at = update_at;
+    }
+    
+    
     
 }
--- BBDD/universidad/src/main/java/com/prog/Estudiante.java
+++ BBDD/universidad/src/main/java/com/prog/Estudiante.java
@@ -1,6 +1,121 @@
 package com.prog;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Estudiante {
-    private String nombre;
-    private Curso curso;
+    private int id;
+    private String matricula, nombre, apellidos, email, tlf;
+    private LocalDate fecha_nac;
+    private boolean activo;
+    private LocalDateTime created_at, update_at;
+    
+    public Estudiante(int id, String matricula, String nombre, String apellidos, String email, String tlf,
+            LocalDate fecha_nac, int activo) {
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.tlf = tlf;
+        this.fecha_nac = fecha_nac;
+        this.activo = true;
+    }
+
+    public Estudiante(int id, String matricula, String nombre, String apellidos, String email, String tlf,
+            LocalDate fecha_nac, boolean activo, LocalDateTime created_at, LocalDateTime update_at) {
+        this.id = id;
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.tlf = tlf;
+        this.fecha_nac = fecha_nac;
+        this.activo = activo;
+        this.created_at = created_at;
+        this.update_at = update_at;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTlf() {
+        return tlf;
+    }
+
+    public void setTlf(String tlf) {
+        this.tlf = tlf;
+    }
+
+    public LocalDate getFecha_nac() {
+        return fecha_nac;
+    }
+
+    public void setFecha_nac(LocalDate fecha_nac) {
+        this.fecha_nac = fecha_nac;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public LocalDateTime getUpdate_at() {
+        return update_at;
+    }
+
+    public void setUpdate_at(LocalDateTime update_at) {
+        this.update_at = update_at;
+    }
+
+    
+
+    
 }
