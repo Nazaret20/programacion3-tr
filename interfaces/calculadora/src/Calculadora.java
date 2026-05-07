457dbe3793422480de9ec660c4ae6c19c3cc691c
Nazaret <nazaretgutirrezmartn@gmail.com>
2026-05-07 08:56:07 +0200

    actualización

--- interfaces/calculadora/src/Calculadora.java
+++ interfaces/calculadora/src/Calculadora.java
@@ -1,15 +1,12 @@
 
-import java.awt.Frame;
-import java.awt.GridLayout;
-import java.awt.TextArea;
-import java.awt.Button;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.*;
+import java.awt.event.*;
 
 public class Calculadora {
-    public static void main(String[] args) {
-        Frame frame = new Frame("Calculadora");
-        TextArea txtResultado = new TextArea();
+    static Frame frame = new Frame("Calculadora");
+
+    public static Panel grid() {
+        Panel panelNums = new Panel(new GridLayout(4, 5));
         Button button1 = new Button("1");
         Button button2 = new Button("2");
         Button button3 = new Button("3");
@@ -22,32 +19,55 @@ public class Calculadora {
         Button button0 = new Button("0");
         Button buttonPunto = new Button(".");
         Button buttonC = new Button("C");
+        panelNums.add(button1);
+        panelNums.add(button2);
+        panelNums.add(button4);
+        panelNums.add(button5);
+        panelNums.add(button3);
+        panelNums.add(button6);
+        panelNums.add(button7);
+        panelNums.add(button8);
+        panelNums.add(button9);
+        panelNums.add(button0);
+        panelNums.add(buttonPunto);
+        panelNums.add(buttonC);
+        return panelNums;
+
+    }
+
+    public static Panel operadores() {
+        Panel panelOper = new Panel(new GridLayout(5, 1));
         Button buttonMas = new Button("+");
         Button buttonMenos = new Button("-");
         Button buttonMult = new Button("*");
         Button buttonDividir = new Button("/");
         Button buttonIgual = new Button("=");
+        buttonMas.setPreferredSize(new Dimension(80, 0));
+        panelOper.add(buttonMas);
+        panelOper.add(buttonMenos);
+        panelOper.add(buttonMult);
+        panelOper.add(buttonDividir);
+        panelOper.add(buttonIgual);
+        return panelOper;
+    }
 
-        frame.setLayout(new GridLayout(4, 4));
+    public static Label resultado() {
+        Label txtResultado = new Label();
+        txtResultado.setFont(new Font("SansSerif", Font.PLAIN, 38));
+        return txtResultado;
+    }
 
-        frame.add(txtResultado);
-        frame.add(button1);
-        frame.add(button2);
-        frame.add(button3);
-        frame.add(button4);
-        frame.add(button5);
-        frame.add(button6);
-        frame.add(button7);
-        frame.add(button8);
-        frame.add(button9);
-        frame.add(button0);
-        frame.add(buttonPunto);
-        frame.add(buttonC);
-        frame.add(buttonMas);
-        frame.add(buttonMenos);
-        frame.add(buttonMult);
-        frame.add(buttonDividir);
-        frame.add(buttonIgual);
+    /*-------------------------------------- */
+    public static void main(String[] args) {
+        frame.setLayout(new BorderLayout());
+
+        Label result = resultado();
+        Panel nums = grid();
+        Panel opers = operadores();
+
+        frame.add(result, BorderLayout.NORTH);
+        frame.add(nums, BorderLayout.CENTER);
+        frame.add(opers, BorderLayout.EAST);
 
         frame.setSize(400, 500);
         frame.setLocationRelativeTo(null);
@@ -60,4 +80,3 @@ public class Calculadora {
         });
     }
 }
-        
