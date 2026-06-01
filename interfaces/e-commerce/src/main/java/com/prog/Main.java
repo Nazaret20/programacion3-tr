9b7ba38ff2699a339606d0cdaf658a7ffba922a0
Nazaret <nazaretgutirrezmartn@gmail.com>
2026-06-01 19:53:48 +0200

    panel izquierdo

--- interfaces/e-commerce/src/main/java/com/prog/Main.java
+++ interfaces/e-commerce/src/main/java/com/prog/Main.java
@@ -1,6 +1,11 @@
 package com.prog;
 
+import javax.swing.SwingUtilities;
+
+import com.prog.vista.VentanaInicio;
+
 public class Main {
     public static void main(String[] args) {
+        VentanaInicio inicio = new VentanaInicio();
     }
 }
--- interfaces/e-commerce/src/main/java/com/prog/vista/VentanaInicio.java
+++ interfaces/e-commerce/src/main/java/com/prog/vista/VentanaInicio.java
@@ -1,9 +1,37 @@
 package com.prog.vista;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class VentanaInicio extends JFrame {
-    VentanaInicio() {
-        
+    public VentanaInicio() {
+        setTitle("E-Commerce");
+        setLayout(new BorderLayout());
+
+        //Panel izquierdo
+        JPanel izq = new JPanel();
+        izq.setPreferredSize(new Dimension(220, 720));
+        izq.setBackground(new Color(129, 215, 255));
+
+
+        JMenuBar menu = new JMenuBar();
+        menu.setLayout(new GridLayout(3, 1, 10, 10));
+        JMenuItem menuItem1 = new JMenuItem("Productos");
+        JMenuItem menuItem2 = new JMenuItem("Clientes");
+        JMenuItem menuItem3 = new JMenuItem("Pedidos");
+
+        menu.add(menuItem1);
+        menu.add(menuItem2);
+        menu.add(menuItem3);
+
+        izq.add(menu);
+
+        add(izq, BorderLayout.WEST);
+
+        setSize(1000, 800);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
